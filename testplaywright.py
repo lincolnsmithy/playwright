d023b60c9aa490628a516cf14d21719e493fecda
jraymond <john.raymond@sap.com>
2021-02-09 20:00:52 -0500

    removed chrome

--- testplaywright.py
+++ testplaywright.py
@@ -2,7 +2,7 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    for browser_type in [p.chromium, p.firefox, p.webkit]:
+    for browser_type in [p.firefox, p.webkit]:
         browser = browser_type.launch()
         page = browser.new_page()
         page.goto('https://cgepreview.concursolutions.com')
