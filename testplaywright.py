4aa2cb3f55160768ad0cdb9e502dc2de0e94c02a
jraymond <john.raymond@sap.com>
2021-02-09 20:50:21 -0500

    added chromium

--- testplaywright.py
+++ testplaywright.py
@@ -2,7 +2,7 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    for browser_type in [p.webkit]:
+    for browser_type in [p.chromium]:
         browser = browser_type.launch()
         page = browser.new_page()
         page.goto('https://cgepreview.concursolutions.com')
