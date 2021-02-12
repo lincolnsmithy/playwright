ff3c460457d9ee255109800b47204fb69ff00bd5
jraymond <john.raymond@sap.com>
2021-02-12 12:13:20 -0500

    cleaned up login and logout

--- cge_login_logout.py
+++ cge_login_logout.py
@@ -3,19 +3,10 @@ from playwright.sync_api import sync_playwright
 import os
 import pytest
 
-#@pytest.fixture(scope='module')
-#def get_sut(self):
-#    self.UN = os.environ['USERNAME']
-#    self.pw = os.environ['PW']
-#    self.testenv = os.environ['ENV']
-
-
 testenv = "https:\\cgepreview.concursolutions.com"
 #testenv = os.environ['ENV']
 #@pytest.fixture(scope='module')
 #@pytest.fixture()
-
-
 
 def log_request(intercepted_request):
     print("a request was made:", intercepted_request.url)
@@ -23,10 +14,10 @@ def log_request(intercepted_request):
 def log_failed_request(intercepted_request):
     print("FAILED REQUEST: ",intercepted_request.url)
 
-def log_requestinfo(intercepted_requestinfo):
+#def log_requestinfo(intercepted_requestinfo):
     #print("requestinfo: ", intercepted_requestinfo.headers["x-edgeconnect-origin-mex-latency"])
-    if "x-edgeconnect-origin-mex-latency" in intercepted_requestinfo.headers.keys():
-        print("x-edgeconnect-origin-mex-latency: ",intercepted_requestinfo.headers["x-edgeconnect-origin-mex-latency"])
+#    if "x-edgeconnect-origin-mex-latency" in intercepted_requestinfo.headers.keys():
+#        print("x-edgeconnect-origin-mex-latency: ",intercepted_requestinfo.headers["x-edgeconnect-origin-mex-latency"])
 
 def test_cge_session(page):
     #p = sync_playwright().start()
@@ -34,7 +25,7 @@ def test_cge_session(page):
 
     page.on("request", log_request)
     page.on("failedrequest", log_failed_request)
-    page.on("response", log_requestinfo)
+    #page.on("response", log_requestinfo)
 
     page.goto(testenv)
     assert page.wait_for_selector("data-test=app-footer-links"), "TEST RESULT"
@@ -51,7 +42,7 @@ def test_cge_session(page):
     print(page.context.cookies())
 
     print("logout")
-    page.set_default_timeout(5000) #Set to handle gateway time out
+    page.set_default_timeout(10000) #Set to handle gateway time out
 
     page.click("data-test=menu-profile")
     page.click("data-test=user-profile-menu-signout-link")
